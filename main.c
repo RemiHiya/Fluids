d8c582be6680249d183ceb2df762f80d3584de4a
reschirra <remi.schirra@etu.uca.fr>
2024-06-02 20:56:20 +0200

    Display optimization

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "src/include/simulation.h"
 
 const int screenWidth = 800;
-const int screenHeight = 450;
-const float resolution = 0.1f;
+const int screenHeight = 400;
+const float resolution = 0.5f;
 
 
 int main(void)
@@ -19,6 +20,9 @@ int main(void)
         BeginDrawing();
         ClearBackground(BLACK);
         display(buffer);
+        char *t = malloc(16 * sizeof(char));
+        sprintf(t, "%f", 1.0f/GetFrameTime());
+        DrawText(t, 0, 0, 15, RED);
         EndDrawing();
     }
 
--- src/utils.c
+++ src/utils.c
@@ -14,12 +14,7 @@ double cosd(double x) {
 void drawPixel(int x, int y, float res, Color color) {
     if (res > 1) return;
     int size = 1 / res;
-
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            DrawPixel((x*size)+j, (y*size)+i, color);
-        }
-    }
+    DrawRectangle(x*size, y*size, size, size, color);
 }
 
 Color dir2col(double x, double y) {
